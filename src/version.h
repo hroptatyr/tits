hroptatyr/blpcli

initial import of toolchain

src/version.h
@@ -1,0 +1,7 @@
+#ifndef INCLUDED_version_h_
+#define INCLUDED_version_h_
+
+extern const char nmck_version_string[];
+#define package_version	nmck_version_string
+
+#endif	/* INCLUDED_version_h_ */

hroptatyr/tits

src/version.h
@@ -1,0 +1,7 @@
+#ifndef INCLUDED_version_h_
+#define INCLUDED_version_h_
+
+extern const char cots_version_string[];
+#define package_version	cots_version_string
+
+#endif	/* INCLUDED_version_h_ */