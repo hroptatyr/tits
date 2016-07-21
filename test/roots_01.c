31a76cd5b85452c5c40450ccfa3af0e8f5f6847f
Sebastian Freundt <freundt@ga-group.nl>
2016-07-21 14:41:21 +0000

    test, provide regression tests for tits_droots() and tits_sroots()

---
+++ test/roots_01.c
@@ -0,0 +1,21 @@
+#include <stddef.h>
+#include <math.h>
+#include "roots.h"
+#include "nifty.h"
+
+
+int main(void)
+{
+	double r[3U] = {NAN, NAN, NAN};
+	double p[] = {140, -13, -8, 1};/* == (x+4)(x-5)(x-7) */
+	const int ref[] = {
+		-4000, 7000, 5000,
+	};
+	int rc = 0;
+
+	rc = tits_droots(r, p, countof(p) - 1U) != 3;
+	for (size_t i = 0U; i < countof(r); i++) {
+		rc |= (int)trunc(r[i] * 1000) != ref[i];
+	}
+	return rc;
+}
