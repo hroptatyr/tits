a212ac1dd9592a72ca3a033de62961da75be54e5
Sebastian Freundt <freundt@ga-group.nl>
2017-07-28 12:04:12 +0000

    minor, implement Hayashi-Yoshida covariance

---
+++ src/hy.h
@@ -0,0 +1,55 @@
+/*** hy.h -- cross correlation for irregular timeseries
+ *
+ * Copyright (C) 2015-2017 Sebastian Freundt
+ *
+ * Author:  Sebastian Freundt <freundt@ga-group.nl>
+ *
+ * This file is part of tits.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * 3. Neither the name of the author nor the names of any contributors
+ *    may be used to endorse or promote products derived from this
+ *    software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED.  IN NO EVENT SHALL THE REGENTS OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+ * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ * OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+ * IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ ***/
+#if !defined INCLUDED_hy_h_
+#define INCLUDED_hy_h_
+#include "tits.h"
+
+
+/* public API */
+/**
+ * Return NLAGS lag values in TGT (which should be 2*NLAGS + 1 wide)
+ * for timeseries TS1 and TS2 and resolution TAU. */
+extern int
+tits_dhy(double *restrict tgt, dts_t ts1, dts_t ts2, int nlags, double tau);
+
+/**
+ * Return NLAGS lag values in TGT (which should be 2*NLAGS + 1 wide)
+ * for timeseries TS1 and TS2 and resolution TAU. */
+extern int
+tits_shy(float *restrict tgt, sts_t ts1, sts_t ts2, int nlags, float tau);
+
+#endif	/* INCLUDED_hy_h_ */
